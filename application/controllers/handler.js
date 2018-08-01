32fd05e33514a34d5f750e565abb2619c1d5020d
panayiotis96 <panayiotisaristidou@hotmail.com>
2018-08-01 14:33:04 +0100

    Set up microservice, retrieve slide html code by slide id and anaalyse it for missing h3 and image alt text

--- application/controllers/handler.js
+++ application/controllers/handler.js
@@ -6,46 +6,77 @@ Handles the requests by executing stuff and replying to the client. Uses promise
 'use strict';
 
 const boom = require('boom'), //Boom gives us some predefined http codes and proper responses
-  slideDB = require('../database/slideDatabase'), //Database functions specific for slides
-  co = require('../common');
+slideDB = require('../database/slideDatabase'), //Database functions specific for slides
+co = require('../common');
+const request = require('request');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+var EventEmitter = require("events").EventEmitter;
 
 module.exports = {
-  //Get slide from database or return NOT FOUND
-  getSlide: function(request, reply) {
-    slideDB.get(encodeURIComponent(request.params.id)).then((slide) => {
-      if (co.isEmpty(slide))
-        reply(boom.notFound());
-      else
-        reply(co.rewriteID(slide));
-    }).catch((error) => {
-      request.log('error', error);
-      reply(boom.badImplementation());
-    });
+
+  //Check a slide for accessibility errors
+  checkSlide: function(input, reply) {
+      var getSlide = new EventEmitter();
+      var html;
+
+      var slide_id = input.payload.slide;
+      //694700-5
+      //757435-2 ->Testing slide
+      request('https://deckservice.slidewiki.org/slide/'+slide_id, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        getSlide.data = body.revisions[0].content;
+        getSlide.emit('received');
+
+      });
+
+      getSlide.on('received', function () {
+         html = getSlide.data;
+         var res = [];
+         var image = [];
+         const dom = new JSDOM('<!DOCTYPE html><html><head><title>titleTest</title></head><body>'+html+'</body></html>');
+
+         //array for storing the accessibility check results
+          var results = [];
+
+//*-----------------------------------------------H3 TAG-----------------------------------
+
+           var has_h3 = {type: "h3", result: (dom.window.document.querySelector("h3").textContent) ? "has h3" : "no h3"};
+           results.push(has_h3);
+//*-------------------------------------------END OF IMG TAG-----------------------------------
+
+//*-----------------------------------------------IMG TAG-----------------------------------
+           //count number of img tags
+           var images_found = dom.window.document.getElementsByTagName("img").length;
+
+           if (images_found!=0){
+             for (var counter=0; counter<images_found; counter++){
+                 var id = dom.window.document.querySelectorAll("img")[counter].id;
+                 var has_alt = (dom.window.document.querySelectorAll("img")[counter].alt) ? "has alt" : "no alt";
+                 var image = {type: "image", id : id, result: has_alt};
+
+                 results.push(image);
+             }
+           }
+//*-------------------------------------------END OF IMG TAG-----------------------------------
+
+//*-----------------------------------------------Table TAG-----------------------------------
+
+//*-------------------------------------------END OF TABLE TAG-----------------------------------
+
+           reply(results);
+
+      });
+
   },
 
-  //Create Slide with new id and payload or return INTERNAL_SERVER_ERROR
-  newSlide: function(request, reply) {
-    slideDB.insert(request.payload).then((inserted) => {
-      if (co.isEmpty(inserted.ops[0]))
-        throw inserted;
-      else
-        reply(co.rewriteID(inserted.ops[0]));
-    }).catch((error) => {
-      request.log('error', error);
-      reply(boom.badImplementation());
-    });
-  },
 
-  //Update Slide with id id and payload or return INTERNAL_SERVER_ERROR
-  replaceSlide: function(request, reply) {
-    slideDB.replace(encodeURIComponent(request.params.id), request.payload).then((replaced) => {
-      if (co.isEmpty(replaced.value))
-        throw replaced;
-      else
-        reply(replaced.value);
-    }).catch((error) => {
-      request.log('error', error);
-      reply(boom.badImplementation());
-    });
-  },
+
+
+
+
+
+
+
+
 };
--- application/server.js
+++ application/server.js
@@ -12,11 +12,12 @@ const hapi = require('hapi'),
 //Initiate the webserver with standard or given port
 const server = new hapi.Server({ connections: {routes: {validate: { options: {convert : false}}}}});
 
-let port = (!co.isEmpty(process.env.APPLICATION_PORT)) ? process.env.APPLICATION_PORT : 3000;
+let port = (!co.isEmpty(process.env.APPLICATION_PORT)) ? process.env.APPLICATION_PORT : 4000;
 server.connection({
   port: port
 });
-let host = (!co.isEmpty(process.env.VIRTUAL_HOST)) ? process.env.VIRTUAL_HOST : server.info.host;
+ let host = (!co.isEmpty(process.env.VIRTUAL_HOST)) ? process.env.VIRTUAL_HOST : 'localhost:4000';
+//let host = (!co.isEmpty(process.env.VIRTUAL_HOST)) ? process.env.VIRTUAL_HOST : server.info.host;
 
 //Export the webserver to be able to use server.log()
 module.exports = server;
